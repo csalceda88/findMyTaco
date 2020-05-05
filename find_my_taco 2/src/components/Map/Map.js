5823f6d01941888678c8d0de8c495d819f162d1a
Veneranda <hveneranda@yahoo.com>
2020-05-05 03:19:31 -0500

    iframe/mapapi

--- find_my_taco 2/src/components/Map/Map.js
+++ find_my_taco 2/src/components/Map/Map.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import './Map.css';
 
-class App extends Component {
+const mapStyles = {
+    width: '50%',
+    height: '50%',
+  };
+class something extends Component {
     render() {
         return (
-        <div>
+            <div>
+            <Map
+            google={this.props.google}
+            zoom={8}
+            style={mapStyles}
+            initialCenter={{ lat: 47.444, lng: -122.176}}
+          >
+              <Marker position={{ lat: 48.00, lng: -122.00}} />
+        </Map>
         </div>
         );
     }
