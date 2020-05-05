719ea4ed590528ec4a4f0ed3b385e98c778835e1
Veneranda <hveneranda@yahoo.com>
2020-05-05 00:11:24 -0500

    compita

---
+++ find_my_taco 2/src/components/Profile/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div class="card">
+              <img src="https://i.imgur.com/7PpQOks.jpg" alt="John" height="276" />
+              <h1>Jose_CaliBoy</h1>
+              <p class="title">LEVEL: Compita</p>
+              <p></p>
+              <p><button>LogOut</button></p>
+              </div>
+            );
+}
+
+export default Profile;
